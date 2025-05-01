b0414fc3117907e6de9968a3ee8b5537c0a8e86e
ssadar1432 <ssadar64@gmail.com>
2025-05-01 12:35:27 +0530

    Initial commit

---
+++ src/main/java/com/svip/InterviewPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.svip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPracticeApplication.class, args);
+	}
+
+}
