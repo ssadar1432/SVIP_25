d158d1387c1364c0a1f1e03e4cb1e4ba3d8e4ff7
ADMIN <jagadalevidyadhar@outlook.com>
2025-05-01 17:03:50 +0530

    two Program

---
+++ src/main/java/com/svip/VIP/Day1/EvenOdd.java
@@ -0,0 +1,11 @@
+package com.svip.VIP.Day1;
+
+public class EvenOdd {
+
+    public void evenOdd(Integer num){
+
+        String result=(num)%2==0?"even":"odd";
+
+        System.out.println("The number is "+num+" is "+result+" . ");
+    }
+}
---
+++ src/main/java/com/svip/VIP/Day1/PalindromeChecker.java
@@ -0,0 +1,9 @@
+package com.svip.VIP.Day1;
+
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+
+    }
+
+}
